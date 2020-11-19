6abe3936c0ecbda257cd974c6bcf5dbe6cd47d21
이종호 <jhleed3@gmail.com>
2020-11-19 22:53:45 +0900

    스크립트 시나리오

---
+++ src/myscript.js
@@ -0,0 +1,23 @@
+//받는사람 이름
+document.querySelector("input[name=receiver_name]").value = ""
+
+//받는사람 전화번호
+document.querySelector("input[name=receiver_tel11]").value = ""
+
+//받는사람 핸드폰
+document.querySelector("input[name=receiver_tel21]").value = ""
+
+//직접 입력 클릭
+document.querySelector("input[type=radio][value=M]").click();
+
+//우편번호
+document.querySelector("input[name=rpost]").value = ""
+
+//기본주소
+document.querySelector("input[name=raddr1]").value = ""
+
+//상세주소
+document.querySelector("input[name=raddr2]").value = ""
+
+//적립금 사용 클릭
+document.querySelector("a img[src='/V2/_skin/ownerclan_v2/_img/order/btn_reservecopy.gif'").click()
